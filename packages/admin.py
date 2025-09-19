921cd496a2439a2e6aca39c0d3fabf6615db1fdd
CodeBlueJay <126738791+CodeBlueJay@users.noreply.github.com>
2025-09-19 06:48:16 +0000

    Fixed thingy where everyone could use admin give

--- packages/admin.py
+++ packages/admin.py
@@ -27,5 +27,8 @@ async def roll10(interaction: discord.Interaction):
 
 @admin_group.command(name="give", description="Give an item to a user")
 async def give(interaction: discord.Interaction, user: discord.User, item: str):
+    if interaction.user.id != 908954867962380298:
+        await interaction.response.send_message("You are not allowed to use this command!", ephemeral=True)
+        return
     await add_to_inventory(item.title(), user.id)
     await interaction.response.send_message(f"Gave **{item.title()}** to {user.name}!")
